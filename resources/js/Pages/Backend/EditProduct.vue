34be00d1412663e4f4b819c7de4402fb13f40083
hidecard <hidecard1500@gmail.com>
2025-07-06 06:59:19 +0630

    product

--- resources/js/Pages/Backend/EditProduct.vue
+++ resources/js/Pages/Backend/EditProduct.vue
@@ -2,23 +2,70 @@
 import { onMounted, reactive, ref, watch } from 'vue';
 import MasterBackend from './Layout/MasterBackend.vue';
 import { useForm } from '@inertiajs/vue3';
+import { toast } from 'vue3-toastify';
+
 defineOptions({
     layout: MasterBackend
-})
+});
 
+const props = defineProps({
+    product: Object,
+    categories: Array
+});
+
+const editProductForm = useForm({
+    name: props.product.name,
+    category_id: props.product.category_id,
+    weight: props.product.weight,
+    price: props.product.price,
+    sale_price: props.product.sale_price,
+    tag: props.product.tag || "",
+    status: props.product.status == 1,
+    slug: props.product.slug,
+    images: [],
+});
+
+const currentImages = JSON.parse(props.product.image);
+const newImages = ref([]);
+
+function handleFileInput(event) {
+    newImages.value = [];
+    const files = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+        const reader = new FileReader();
+        reader.onload = () => {
+            newImages.value.push(reader.result);
+        };
+        reader.readAsDataURL(files[i]);
+    }
+    editProductForm.images = Array.from(event.target.files);
+}
+
+const submit = () => {
+    editProductForm.post(route('dash.product.edit', props.product.id), {
+        forceFormData: true,
+        onSuccess: () => {
+            toast.success('Product Updated');
+        },
+        onError: () => {
+            if (editProductForm.errors.message)
+                toast.error(editProductForm.errors.message);
+        },
+    });
+};
 </script>
 
 <template>
     <main class="main-content-wrapper">
         <div class="container">
             <!-- Page Header -->
-            <div class="row mb-8">
+            <div class="mb-8 row">
                 <div class="col-md-12">
                     <div class="d-md-flex justify-content-between align-items-center">
                         <div>
                             <h2>Edit Product</h2>
                             <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb mb-0">
+                                <ol class="mb-0 breadcrumb">
                                     <li class="breadcrumb-item">
                                         <Link :href="route('dashboard')" class="text-inherit">Dashboard</Link>
                                     </li>
@@ -39,100 +86,108 @@ defineOptions({
             <!-- Product Form -->
             <div class="row">
                 <div class="col-lg-8 col-12">
-                    <div class="card mb-6 card-lg">
-                        <div class="card-body p-6">
+                    <div class="mb-6 card card-lg">
+                        <div class="p-6 card-body">
                             <h4 class="mb-4 h5">Product Information</h4>
-                            <form>
+                            <form @submit.prevent="submit">
                                 <div class="row">
                                     <!-- Title -->
                                     <div class="mb-3 col-lg-6">
                                         <label class="form-label">Title</label>
-                                        <input type="text" class="form-control" placeholder="Product Name" name="name"/>
-                                        <div class="text-danger">Error tag goes here</div>
-
+                                        <input type="text" class="form-control" placeholder="Product Name" name="name"
+                                            v-model="editProductForm.name" />
+                                        <div class="text-danger">{{ editProductForm.errors.name }}</div>
                                     </div>
 
                                     <!-- Category -->
                                     <div class="mb-3 col-lg-6">
                                         <label class="form-label">Product Category</label>
-                                        <select class="form-select"
+                                        <select v-model="editProductForm.category_id" class="form-select"
                                             name="category_id">
-                                            <option>Product Category</option>
-
+                                            <option disabled value="">Product Category</option>
+                                            <option v-for="category in categories" :key="category.id"
+                                                :value="category.id">
+                                                {{ category.name }}
+                                            </option>
                                         </select>
-                                        <div class="text-danger">error tags goes here</div>
-
+                                        <div class="text-danger">{{ editProductForm.errors.category_id }}</div>
                                     </div>
-
 
                                     <!-- Weight -->
                                     <div class="mb-3 col-lg-6">
                                         <label class="form-label">Weight</label>
-                                        <input type="text" class="form-control" placeholder="Weight" name="weight"/>
-                                        <div class="text-danger">error tags goes here</div>
-
+                                        <input type="text" class="form-control" placeholder="Weight" name="weight"
+                                            v-model="editProductForm.weight" />
+                                        <div class="text-danger">{{ editProductForm.errors.weight }}</div>
                                     </div>
+
+                                    <!-- Slug -->
                                     <div class="mb-3 col-lg-6">
                                         <label class="form-label">Slug</label>
-                                        <input type="text" class="form-control" placeholder="Weight" name="slug"/>
-                                        <div class="text-danger">error tags goes here</div>
-
+                                        <input type="text" class="form-control" placeholder="Slug" name="slug"
+                                            v-model="editProductForm.slug" />
+                                        <div class="text-danger">{{ editProductForm.errors.slug }}</div>
                                     </div>
 
+                                    <!-- Status -->
                                     <div class="mb-3 col-lg-12">
                                         <label class="form-label">Status</label>
                                         <div class="form-check form-switch">
-                                            <input class="form-check-input"
+                                            <input v-model="editProductForm.status" class="form-check-input"
                                                 type="checkbox" name="status" id="flexCheckDefault" />
                                             <label class="form-check-label" for="flexCheckDefault">Active</label>
                                         </div>
                                     </div>
 
                                     <!-- Product Images -->
-                                    <div class="mb-3 col-lg-12 mt-5">
+                                    <div class="mt-5 mb-3 col-lg-12">
                                         <h4 class="mb-3 h5">Product Images</h4>
-                                        <div class="d-flex gap-2" id="previewImg">
-                                            <img src="" class="w-25 h-25" />
-
-                                            <img src="" alt=""class="w-25 h-25">
+                                        <div class="flex-wrap gap-2 d-flex" id="previewImg">
+                                            <template v-if="newImages.length">
+                                                <img v-for="(image, index) in newImages" :key="'new-' + index" :src="image"
+                                                    class="w-25 h-25" />
+                                            </template>
+                                            <template v-else>
+                                                <img v-for="(image, index) in currentImages"
+                                                    :key="'current-' + index"
+                                                    :src="'/images/products/' + image"
+                                                    class="w-25 h-25" />
+                                            </template>
                                         </div>
                                         <label for="attachment"
-                                            class="form-label border border-2 rounded mt-3 p-3 text-center d-col"
+                                            class="p-3 mt-3 text-center border border-2 rounded form-label d-col"
                                             style="border-style: dashed!important;">
-                                            <img src="" class="w-25" alt="">
+                                            <img :src="'/images/cloud-upload-svgrepo.svg'" class="w-25" alt="">
                                             <div>Attach Media</div>
                                         </label>
-                                        <input type="file" accept="image/*" name="image[]"
+                                        <input @input="handleFileInput" type="file" accept="image/*" name="image[]"
                                             id="attachment" class="col-md-5 col-sm-10 d-none" multiple>
-                                        <div class="text-danger">error tags goes here</div>
                                     </div>
 
-                                    <!-- Product Price, Sale Price, Quantity, Code, SKU -->
-                                    <div class="mb-3 col-lg-12 mt-5">
+                                    <!-- Product Price, Sale Price, Tag -->
+                                    <div class="mt-5 mb-3 col-lg-12">
                                         <div class="mb-3">
                                             <label class="form-label">Product Price</label>
                                             <input type="text" class="form-control" placeholder="Enter Product Price"
-                                                name="price"/>
-                                            <div class="text-danger">error tags goes here</div>
-
+                                                name="price" v-model="editProductForm.price" />
+                                            <div class="text-danger">{{ editProductForm.errors.price }}</div>
                                         </div>
+
                                         <div class="mb-3">
                                             <label class="form-label">Sale Price</label>
                                             <input type="text" class="form-control" placeholder="Enter Sale Price"
-                                                name="sale_price"/>
-                                            <div class="text-danger">error tags goes here</div>
-
+                                                name="sale_price" v-model="editProductForm.sale_price" />
+                                            <div class="text-danger">{{ editProductForm.errors.sale_price }}</div>
                                         </div>
 
                                         <div class="mb-3">
                                             <label class="form-label">Tag</label>
-                                            <select class="form-select" name="tag">
+                                            <select v-model="editProductForm.tag" class="form-select" name="tag">
                                                 <option value="">Select Tag</option>
                                                 <option value="Hot">Hot</option>
                                                 <option value="Sale">Sale</option>
                                             </select>
-                                            <div class="text-danger">error tags goes here</div>
-
+                                            <div class="text-danger">{{ editProductForm.errors.tag }}</div>
                                         </div>
                                     </div>
 
--- resources/js/Pages/Backend/Products.vue
+++ resources/js/Pages/Backend/Products.vue
@@ -8,11 +8,49 @@ const props = defineProps({
     products: Array,
 })
 
+//implemented search and status filter
+import { computed, ref } from 'vue';
+const search = ref('');
+const selectedStatus = ref('');
+
+const filteredProducts = computed(() => {
+    let newProducts = props.products.data;
+
+    if (search.value) {
+        const searchText = search.value.toLowerCase();
+        newProducts = newProducts.filter(product => {
+            return product.name.toLowerCase().includes(searchText) ||
+                product.category.name.toLowerCase().includes(searchText);
+        });
+    }
+
+    if (selectedStatus.value != '') {
+        newProducts = newProducts.filter(product => {
+            return product.status == selectedStatus.value;
+        });
+    }
+
+    return newProducts;
+});
+
+//product item delete function
+import { toast } from 'vue3-toastify';
+import { useForm } from '@inertiajs/vue3';
+function deleteProduct(product_id) {
+    useForm({}).get(route('dash.product.delete', product_id), {
+        onSuccess: () => {
+            toast.success('Product deleted successfully');
+        },
+        onError: () => {
+            toast.error('Failed to delete product');
+        }
+    })
+}
 </script>
 <template>
     <main class="main-content-wrapper">
         <div class="container">
-            <div class="row mb-8">
+            <div class="mb-8 row">
                 <div class="col-md-12">
                     <!-- page header -->
                     <div class="d-md-flex justify-content-between align-items-center">
@@ -20,7 +58,7 @@ const props = defineProps({
                             <h2>Products</h2>
                             <!-- breacrumb -->
                             <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb mb-0">
+                                <ol class="mb-0 breadcrumb">
                                     <li class="breadcrumb-item">
                                         <Link :href="route('dashboard')" class="text-inherit">Dashboard</Link>
                                     </li>
@@ -35,15 +73,15 @@ const props = defineProps({
                     </div>
                 </div>
             </div>
-            <!-- row -->
+            <!-- row --> 
             <div class="row">
-                <div class="col-xl-12 col-12 mb-5">
+                <div class="mb-5 col-xl-12 col-12">
                     <!-- card -->
                     <div class="card h-100 card-lg">
                         <div class="px-6 py-6">
                             <div class="row justify-content-between">
                                 <!-- form -->
-                                <div class="col-lg-4 col-md-6 col-12 mb-2 mb-lg-0">
+                                <div class="mb-2 col-lg-4 col-md-6 col-12 mb-lg-0">
                                     <form class="d-flex" role="search">
                                         <input v-model="search" class="form-control" type="search"
                                             placeholder="Search Products" aria-label="Search" />
@@ -60,11 +98,11 @@ const props = defineProps({
                             </div>
                         </div>
                         <!-- card body -->
-                        <div class="card-body p-0">
+                        <div class="p-0 card-body">
                             <!-- table -->
                             <div class="table-responsive">
                                 <table
-                                    class="table table-centered table-hover text-nowrap table-borderless mb-0 table-with-checkbox">
+                                    class="table mb-0 table-centered table-hover text-nowrap table-borderless table-with-checkbox">
                                     <thead class="bg-light">
                                         <tr>
                                             <th>
@@ -84,7 +122,7 @@ const props = defineProps({
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
+                                        <tr v-for="product in filteredProducts" :key="product.id">
                                             <td>
                                                 <div class="form-check">
                                                     <input class="form-check-input" type="checkbox" value=""
@@ -93,14 +131,25 @@ const props = defineProps({
                                                 </div>
                                             </td>
                                             <td>
-                                                <a href="#"><img src="/public/images/products/product-img-11.jpg" alt="" class="icon-shape icon-md" /></a>
+                                                <a href="#"><img
+                                                        :src="`/images/products/${JSON.parse(product.image)[0]}`" alt=""
+                                                        class="icon-shape icon-md" /></a>
                                             </td>
-                                            <td> Apple </td>
-                                            <td> Fruits </td>
+                                            <td>
+                                                <Link
+                                                    :href="route('product.view', [product.category.slug, product.slug])"
+                                                    class="text-reset">{{ product.name }}</Link>
+                                            </td>
+                                            <td>{{ product.category.name }}</td>
 
-                                            <td><span>Published</span></td>
-                                            <td>$500</td>
-                                            <td>2025-01-05</td>
+                                            <td>
+                                                <span v-if="product.status"
+                                                    class="badge bg-light-primary text-dark-primary">Published</span>
+                                                <span v-else
+                                                    class="badge bg-light-danger text-dark-danger">Unpublished</span>
+                                            </td>
+                                            <td>{{ product.price }}</td>
+                                            <td>{{ new Date(product.updated_at).toLocaleString() }}</td>
                                             <td>
                                                 <div class="dropdown">
                                                     <a href="#" class="text-reset" data-bs-toggle="dropdown"
@@ -109,14 +158,14 @@ const props = defineProps({
                                                     </a>
                                                     <ul class="dropdown-menu">
                                                         <li>
-                                                            <a class="dropdown-item">
+                                                            <a class="dropdown-item" @click="deleteProduct(product.id)">
                                                                 <i class="bi bi-trash me-3"></i>
                                                                 Delete
                                                             </a>
                                                         </li>
                                                         <li>
                                                             <Link class="dropdown-item"
-                                                                :href="route('dash.product.edit', 1)">
+                                                                :href="route('dash.product.edit', product.id)">
                                                             <i class="bi bi-pencil-square me-3"></i>
                                                             Edit
                                                             </Link>
