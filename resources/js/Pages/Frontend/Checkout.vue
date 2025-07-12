454c11cac462925398669654967a8065673065c5
hidecard <hidecard1500@gmail.com>
2025-07-12 08:36:39 +0630

    checkout

--- resources/js/Pages/Frontend/Checkout.vue
+++ resources/js/Pages/Frontend/Checkout.vue
@@ -7,19 +7,18 @@ defineOptions({
     layout: MasterFrontend,
 })
 
+
 const props = defineProps({
-    user : Object,
-    homeAdress : Object,
-    officeAddress : Object,
-    paymentOptions : Array
+    user: Object,
+    homeAddress: Object,
+    officeAddress: Object,
+    paymentOptions: Array,
 })
 
 import AddressModal from './Components/AddressModal.vue';
-import { toast } from 'vue3-toastify';
 const showModal = ref(false);
 const newProps = ref('');
-
-const openModel =  (types) => {
+const openModal = (types) => {
     if (types == 'new') newProps.value = {};
     else if (types == 'editHome') newProps.value = props.homeAddress;
     else newProps.value = props.officeAddress;
@@ -27,29 +26,70 @@ const openModel =  (types) => {
     showModal.value = true;
 };
 
-
-let updateAddress = useForm({});
-
-function updateDefault(){
-    updateAddress.post(route('addressDefault.update'), {
+let addressUpdate = useForm({});
+function updateDefault() {
+    addressUpdate.get(route('addressDefault.update'), {
         onSuccess: () => {
-            toast.success("Address Updated");
+            toast.success("Default address updated");
         },
         onError: () => {
-            toast.error(updateAddress.errors.message);
+            toast.error(form.errors.message);
         },
-    })
+    });
 }
 
 function deleteAddress(id) {
     useForm({}).get(route('address.delete', id))
 }
 
-
+//checkout methods
 const isAddress = ref(props.homeAddress?.is_default || props.officeAddress?.is_default);
+watch(() => [props.homeAddress, props.officeAddress], () => {
+    isAddress.value = props.homeAddress?.is_default || props.officeAddress?.is_default;
+});
+const paymentMethod = ref(null);
+const checkOutForm = useForm({
+    products: JSON.parse(localStorage.getItem("cartList")).map(product => {
+        return { id: product.id, quantity: product.quantity }
+    }),
+    payment_method: paymentMethod.value,
+});
+
+function checkout() {
+    loader.show();
+    checkOutForm.post(route('create.invoice'), {
+        preserveScroll: true,
+        onSuccess: () => {
+            if (checkOutForm.payment_method == 'bkash') {
+                props.paymentOptions.value = props.paymentOptions.desc;
+                openPaymentModal();
+            } else if (checkOutForm.payment_method == 'cod') {
+                toast.success('Order Placed!');
+                setTimeout(() => {
+                    window.location.href = route('profile');
+                }, 2000);
+            } else {
+                window.location.href = props.paymentOptions.GatewayPageURL;
+            }
+            checkOutForm.reset();
+            localStorage.removeItem("cartList");
+            props.paymentOptions = "";
+        },
+        onError: () => {
+            toast.error(checkOutForm.errors.message);
+        },
+        onFinish: () => {
+            loader.hide();
+        },
+    });
+}
 
 
-
+import PaymentOptionModal from './Components/PaymentOptionModal.vue';
+const showPaymentModal = ref(false);
+const openPaymentModal = () => {
+    showPaymentModal.value = true;
+};
 </script>
 <template>
     <main>
@@ -82,11 +122,80 @@ const isAddress = ref(props.homeAddress?.is_default || props.officeAddress?.is_d
                                         data-bs-parent="#accordionFlushExample">
                                         <div class="mt-5">
                                             <div class="row">
-                                                <!-- address will goes here  -->
+                                                <div v-if="homeAddress" class="mb-4 col-xl-6 col-lg-12 col-md-6 col-12">
+                                                    <!-- form -->
+                                                    <div class="p-6 card card-body">
+                                                        <div class="mb-4 form-check">
+                                                            <input class="form-check-input" type="radio"
+                                                                name="flexRadioDefault" id="homeRadio"
+                                                                :checked="homeAddress.is_default" />
+                                                            <label class="form-check-label text-dark"
+                                                                for="homeRadio">Home
+                                                            </label>
+                                                        </div>
+                                                        <!-- address -->
+                                                        <address>
+                                                            <strong>{{ user.firstName + ' ' + user.lastName }}</strong>
+                                                            <br />
+                                                            {{ homeAddress.address }},
+                                                            <br />
+                                                            {{ homeAddress.district }}, {{ homeAddress.city }}, {{
+                                                                homeAddress.country }},
+                                                            <br />
+
+                                                            <abbr title="Phone">Phone: {{ user.phone }}</abbr>
+                                                        </address>
+
+                                                        <button @click="updateDefault" v-if="!homeAddress.is_default"
+                                                            class="btn btn-outline-danger btn-sm col-6">Make Default
+                                                            address </button>
+                                                        <div class="mt-4">
+                                                            <a @click="openModal('editHome')"
+                                                                class="text-inherit">Edit</a>
+                                                            <a @click="deleteAddress(homeAddress.id)"
+                                                                class="text-danger ms-3">Delete</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div v-if="officeAddress"
+                                                    class="mb-4 col-xl-6 col-lg-12 col-md-6 col-12">
+                                                    <!-- input -->
+                                                    <div class="p-6 card card-body">
+                                                        <div class="mb-4 form-check">
+                                                            <input class="form-check-input" type="radio"
+                                                                name="flexRadioDefault" id="officeRadio"
+                                                                :checked="officeAddress.is_default" />
+                                                            <label class="form-check-label text-dark"
+                                                                for="officeRadio">Office </label>
+
+                                                        </div>
+                                                        <address>
+                                                            <strong>{{ user.firstName + ' ' + user.lastName }}</strong>
+                                                            <br />
+                                                            {{ officeAddress.address }},
+                                                            <br />
+                                                            {{ officeAddress.district }}, {{ officeAddress.city }}, {{
+                                                                officeAddress.country }},
+                                                            <br />
+
+                                                            <abbr title="Phone">Phone: {{ user.phone }}</abbr>
+                                                        </address>
+                                                        <button @click="updateDefault" v-if="!officeAddress.is_default"
+                                                            class="btn btn-outline-danger btn-sm col-6">Make Default
+                                                            address</button>
+                                                        <div class="mt-4">
+                                                            <a @click="openModal('editOffice')"
+                                                                class="text-inherit">Edit</a>
+                                                            <a @click="deleteAddress(officeAddress.id)"
+                                                                class="text-danger ms-3">Delete</a>
+                                                        </div>
+
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-
+                                    <AddressModal :address="newProps" v-if="showModal" @close="showModal = false" />
                                 </div>
 
 
@@ -109,7 +218,7 @@ const isAddress = ref(props.homeAddress?.is_default || props.officeAddress?.is_d
                                                         <!-- check input -->
                                                         <div class="d-flex">
                                                             <div class="form-check">
-                                                                <input
+                                                                <input v-model="checkOutForm.payment_method"
                                                                     value="others" class="form-check-input" type="radio"
                                                                     name="flexRadioDefault" id="others" />
                                                                 <label class="form-check-label ms-2"
@@ -132,7 +241,7 @@ const isAddress = ref(props.homeAddress?.is_default || props.officeAddress?.is_d
                                                         <!-- check input -->
                                                         <div class="d-flex">
                                                             <div class="form-check">
-                                                                <input value="cod"
+                                                                <input v-model="checkOutForm.payment_method" value="cod"
                                                                     class="form-check-input" type="radio"
                                                                     name="flexRadioDefault" id="cashonDelivery" />
                                                                 <label class="form-check-label ms-2"
@@ -154,8 +263,13 @@ const isAddress = ref(props.homeAddress?.is_default || props.officeAddress?.is_d
                                                         aria-expanded="false" aria-controls="flush-collapseThree">
                                                         Prev
                                                     </a>
-                                                    <button class="btn main-theme ms-2">Place Order</button>
-                                                    <!-- payment method options will go here  -->
+                                                    <button
+                                                        :disabled="checkOutForm.payment_method == null || !isAddress"
+                                                        @click="checkout" class="btn main-theme ms-2">Place
+                                                        Order</button>
+
+                                                    <PaymentOptionModal :paymentOptions="paymentOptions"
+                                                        v-if="showPaymentModal" @close="showPaymentModal = false" />
                                                 </div>
                                             </div>
                                         </div>
@@ -163,7 +277,7 @@ const isAddress = ref(props.homeAddress?.is_default || props.officeAddress?.is_d
                                 </div>
                             </div>
                         </div>
-                        <!-- <CheckoutCart />  will goes here-->
+                        <CheckoutCart />
                     </div>
                 </div>
             </div>
